7a6e48417d8a77605931ea7a05e7d01073b0125c
shikaan <spagnolo.manu@gmail.com>
2024-01-03 21:24:37 +0100

    test: add timing

--- test/run.cpp
+++ test/run.cpp
@@ -1,6 +1,7 @@
 #include <iomanip>
 #include <iostream>
 
+#include "../src/Clock.h"
 #include "EventKeyboard_test.h"
 #include "EventStep_test.h"
 #include "Event_test.h"
@@ -14,43 +15,59 @@
 #include "utils_test.h"
 
 void suite(std::string name) { std::cout << yellow("\n" + name + "\n"); }
+void timing(float delta) { printf("    Duration: %.2fμs\n", delta); }
 
 int main() {
   int result = 0;
+  df::Clock c;
+  c.delta();
+  auto initial = c.getNow();
+
   suite("utils_test");
   result += utils_test();
+  timing(c.delta());
 
   suite("Vector_test");
   result += Vector_test();
+  timing(c.delta());
 
   suite("Object_test");
   result += Object_test();
+  timing(c.delta());
 
   suite("ObjectList_test");
   result += ObjectList_test();
+  timing(c.delta());
 
   suite("ObjectListIterator_test");
   result += ObjectListIterator_test();
+  timing(c.delta());
 
   suite("Event_test");
   result += Event_test();
+  timing(c.delta());
 
   suite("EventStep_test");
   result += EventStep_test();
+  timing(c.delta());
 
   suite("EventKeyboard_test");
   result += EventKeyboard_test();
+  timing(c.delta());
 
   suite("WorldManager_test");
   result += WorldManager_test();
+  timing(c.delta());
+  auto final = c.getNow();
 
   if (result == 0) {
     std::cout << std::endl
-              << green("Success! All " + getAssertions() + " tests passed.")
-              << std::endl;
+              << green("Success! All " + getAssertions() + " tests passed. ");
+    printf("Total Duration: %.2fms\n",
+           static_cast<double>(final - initial) / 1000.0);
   } else {
     std::cout << std::endl
-              << green("Failure. " + std::to_string(result) + " failed tests.")
+              << red("Failure. " + std::to_string(result) + " failed tests.")
               << std::endl;
   }
 

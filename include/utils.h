a919033d3a44864a8ccec64d2b5e9f7b1303538b
shikaan <spagnolo.manu@gmail.com>
2024-01-13 13:43:40 +0100

    chore: update utils headers

--- include/utils.h
+++ include/utils.h
@@ -5,7 +5,7 @@
 #include "Vector.h"
 
 char *getTimeString();
-void sleep(long int milliseconds);
+void sleep(long int microseconds);
 bool match(df::Vector p1, df::Vector p2);
 bool equals(float a, float b, float tolerance = 0.0001f);
 bool intersects(df::Box box1, df::Box box2);

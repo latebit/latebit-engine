c9dcf956f0e90fd971b6197fe457a449969f0e80
shikaan <spagnolo.manu@gmail.com>
2024-01-21 10:16:37 +0100

    feat: introduce subscription into Manager

--- include/Manager.h
+++ include/Manager.h
@@ -1,29 +1,62 @@
 #pragma once
 
+#include <array>
 #include <string>
 
 #include "Event.h"
-#include "Manager.h"
+#include "ObjectList.h"
+
+using namespace std;
 
 namespace df {
+const int MAX_EVENTS = 10;
+
 class Manager {
  private:
-  std::string type;
-  bool is_started;
+  string type = "Manager";
+  bool started = false;
+
+  // Number of events for which there is a subscription
+  int eventCount = 0;
+
+  // The following are parallel lists: for a given i there is
+  // a list subscriber[i] of objects subscribed to the event[i].
+
+  // List of subscriptions. Every element matches an object in `subscribers`
+  array<string, MAX_EVENTS> events = {};
+
+  // List of subscribers. Every element matches an event in `events`
+  array<ObjectList, MAX_EVENTS> subscribers = {};
+
+  // Validates whether a given event can be handled by this Manager
+  // [[nodiscard]] virtual auto isValid(string eventType) const -> bool;
 
  protected:
-  void setType(std::string type);
+  // Sets the current manager type
+  void setType(string type);
 
  public:
   Manager();
   virtual ~Manager();
 
-  std::string getType() const;
-  auto isStarted() const -> bool;
+  // Returns the manager type
+  [[nodiscard]] auto getType() const -> string;
+  // Returns true when the manager has started up
+  [[nodiscard]] auto isStarted() const -> bool;
 
   virtual auto startUp() -> int;
   virtual void shutDown();
 
+  // Broadcasts the event to all the interested subscribers.
+  // Returns count of events sent.
   auto onEvent(const Event *event) const -> int;
+
+  // Register interest in an event type, to be notified when
+  // events of that type occur
+  auto subscribe(Object *o, string eventType) -> int;
+
+  // Unregister interest in a given event type and stop being
+  // notified
+  auto unsubscribe(Object *o, string eventType) -> int;
 };
 };  // namespace df
--- src/Manager.cpp
+++ src/Manager.cpp
@@ -2,38 +2,81 @@
 
 #include "Event.h"
 #include "ObjectListIterator.h"
-#include "WorldManager.h"
 
 namespace df {
 
 void Manager::setType(std::string type) { this->type = type; }
 
-Manager::Manager() {
-  this->is_started = false;
-  this->type = "";
-}
-
+Manager::Manager() = default;
 Manager::~Manager() = default;
 
 auto Manager::getType() const -> std::string { return this->type; }
 
-auto Manager::isStarted() const -> bool { return this->is_started; }
+auto Manager::isStarted() const -> bool { return this->started; }
 
 auto Manager::startUp() -> int {
-  this->is_started = true;
+  this->started = true;
   return 0;
 }
 
-void Manager::shutDown() { this->is_started = false; }
+void Manager::shutDown() { this->started = false; }
+
+auto Manager::subscribe(Object* o, string eventType) -> int {
+  // if (!isValid(eventType)) {
+  //   return -1;
+  // }
+
+  for (int i = 0; i < eventCount; i++) {
+    if (events[i] == eventType) {
+      subscribers[i].insert(o);
+      return 0;
+    }
+  }
+
+  if (eventCount >= MAX_EVENTS) return -1;
+
+  events[eventCount] = eventType;
+  subscribers[eventCount].clear();
+  subscribers[eventCount].insert(o);
+  eventCount++;
+
+  return 0;
+}
+
+auto Manager::unsubscribe(Object* o, string eventType) -> int {
+  int foundIndex = 0;
+  for (int i = 0; i < eventCount; i++) {
+    if (events[i] == eventType) {
+      subscribers[i].remove(o);
+      foundIndex = i;
+      break;
+    }
+  }
+
+  if (subscribers[foundIndex].isEmpty()) {
+    subscribers[foundIndex] = subscribers[eventCount];
+    events[foundIndex] = events[eventCount];
+    eventCount--;
+  }
+
+  return 0;
+}
 
 auto Manager::onEvent(const Event* event) const -> int {
   int count = 0;
 
-  auto objects = WM.getAllObjects();
-  auto iterator = ObjectListIterator(&objects);
-  for (iterator.first(); !iterator.isDone(); iterator.next()) {
-    iterator.currentObject()->eventHandler(event);
-    count++;
+  for (int i = 0; i < eventCount; i++) {
+    auto currentEvent = events[i];
+    if (currentEvent == event->getType()) {
+      auto iterator = ObjectListIterator(&subscribers[i]);
+
+      for (iterator.first(); !iterator.isDone(); iterator.next()) {
+        if (iterator.currentObject() == nullptr) continue;
+
+        iterator.currentObject()->eventHandler(event);
+        count++;
+      }
+    }
   }
 
   return count;

e9daf6887e3e4b98ee8297a90d2c2a206764b90e
shikaan <spagnolo.manu@gmail.com>
2024-01-27 17:28:32 +0100

    doc: document Box

--- include/Box.h
+++ include/Box.h
@@ -4,6 +4,8 @@
 
 #include "Vector.h"
 
+using namespace std;
+
 namespace df {
 class Box {
  private:
@@ -16,19 +18,31 @@ class Box {
 
  public:
   Box();
+
+  // Create box with upper left corner at (0,0), width, and height
   Box(float width, float height);
+
+  // Create box with upper left corner at topLeft, width, and height
   Box(Vector topLeft, float width, float height);
 
+  // Set upper left corner of box
   void setCorner(Vector corner);
+  // Get upper left corner of box
   [[nodiscard]] auto getCorner() const -> Vector;
 
+  // Set width of box
   void setWidth(float width);
+  // Get width of box
   [[nodiscard]] auto getWidth() const -> float;
 
+  // Set height of box
   void setHeight(float height);
+  // Get height of box
   [[nodiscard]] auto getHeight() const -> float;
 
+  // Compare boxes, true if same
   auto operator==(const Box &other) const -> bool;
-  [[nodiscard]] auto toString() const -> std::string;
+  // Represent the box as a string
+  [[nodiscard]] auto toString() const -> string;
 };
 }  // namespace df

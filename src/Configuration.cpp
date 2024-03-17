c1dabc86ecae0f8f220860eba900b2d720d4b5fa
shikaan <spagnolo.manu@gmail.com>
2024-03-17 17:43:23 +0100

    chore: tidy

--- src/Configuration.cpp
+++ src/Configuration.cpp
@@ -62,7 +62,7 @@ auto Configuration::fromFile(string filename) -> int {
     line = getLine(&file);
 
     // Ignore empty lines and comments
-    if (line.empty() || line.starts_with('#')) continue;
+    if (line.empty() || line.at(0) == '#') continue;
 
     string key = trim(line.substr(0, line.find('=')));
     string value = trim(line.substr(line.find('=') + 1));

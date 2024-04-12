8d4a5fe5300bb25e0d0597b29ed1735ad0cdc12f
shikaan <spagnolo.manu@gmail.com>
2024-04-12 22:07:50 +0200

    ci: introduce release steps

--- cmake/Release.cmake
+++ cmake/Release.cmake
@@ -7,11 +7,18 @@ install(DIRECTORY ${SDL2_INCLUDE_DIR} DESTINATION include)
 set(CPACK_PACKAGE_VENDOR "Manuel Spagnolo")
 set(CPACK_PACKAGE_CONTACT "manuelspagnolo@duck.com")
 set(CPACK_PACKAGE_DESCRIPTION "A game engine integrated in your IDE for pixel art games.")
+set(CPACK_PACKAGE_VERSION ${VERSION})
 
 if (WASM)
   set(CPACK_PACKAGE_FILE_NAME "${PROJECT_NAME}-${VERSION_TAG}-wasm")
 else()
   set(CPACK_PACKAGE_FILE_NAME "${PROJECT_NAME}-${VERSION_TAG}-${CMAKE_SYSTEM_NAME}")
+endif()
+
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+  set(CPACK_GENERATOR "DEB;TGZ")
+elseif (APPLE)
+  set(CPACK_GENERATOR "DragNDrop;TGZ")
 endif()
 
 include(CPack)

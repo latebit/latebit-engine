244c52404df6ade104cf1d51d550a83448613b36
Manuel Spagnolo <shikaan@users.noreply.github.com>
2024-04-13 08:34:10 +0200

    ci: use private and explicit include

--- cmake/Native.cmake
+++ cmake/Native.cmake
@@ -6,11 +6,12 @@ find_package(PNG REQUIRED)
 # Create shared library
 add_library(${PROJECT_NAME} SHARED ${SRC_FILES})
 target_link_libraries(${PROJECT_NAME} 
-    PUBLIC
+    PRIVATE
     $<TARGET_NAME_IF_EXISTS:SDL2::SDL2main>
     PNG::PNG
     SDL2::SDL2-static
     SDL2_mixer::SDL2_mixer-static)
+target_include_directories(${PROJECT_NAME} PUBLIC ${SDL2_INCLUDE_DIRS})
 
 # Create test executable
 add_executable(test.out test/main.cpp ${TEST_LIB_FILES})

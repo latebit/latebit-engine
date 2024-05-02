ae7bbbc7604611ba2cc618133265ac92d9e9123d
shikaan <spagnolo.manu@gmail.com>
2024-05-02 12:55:00 +0200

    feat: convert tune to string and don't duplicate upon Continue
    With this change we are changing the meaning of the Continue symbol:
    previously it was meant as a placeholder which would make the parser
    duplicate the previous Note.
    
    Now, it's a symbol by itself and we interpret its meaning only
    in the sequencer: whenever we see a Continue note we keep playing.

--- latebit/sid/parser/parser.h
+++ latebit/sid/parser/parser.h
@@ -10,9 +10,13 @@ using namespace std;
 namespace sid {
 
 class TuneParser {
+ private:
+  static auto fromStream(istream *str) -> unique_ptr<Tune>;
+
  public:
   static auto fromFile(const string filename) -> unique_ptr<Tune>;
-  static auto fromString(istream *str) -> unique_ptr<Tune>;
+  static auto fromString(string str) -> unique_ptr<Tune>;
+  static auto toString(const Tune &tune) -> string;
 };
 
 }  // namespace sid

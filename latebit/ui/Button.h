e04cedc2e11ce3fa910e281b5d9604e9cf24245d
Manuel Spagnolo <manuel.spagnolo@proton.me>
2024-10-26 21:45:09 +0200

    feat: split headers and implementation of button

--- latebit/ui/Button.h
+++ latebit/ui/Button.h
@@ -1,35 +1,25 @@
 #pragma once
 
 #include <functional>
+#include <string>
 
-#include "latebit/core/geometry/Vector.h"
 #include "latebit/core/world/Scene.h"
 #include "latebit/ui/Control.h"
 #include "latebit/ui/Text.h"
 
 namespace lbui {
+
 class Button : public Control {
  private:
   Text* text;
-  function<void()> callback;
+  std::function<void()> callback;
 
  public:
-  Button(Scene* scene, const string label, function<void()> onSubmit) {
-    this->text = scene->createObject<Text>(label, label);
-    this->setBox(this->text->getBox());
-    this->callback = onSubmit;
-  }
-  auto getLabel() -> string override { return this->text->getContent(); }
-  void setLabel(const string l) override {
-    this->text->setContent(l);
-    this->setBox(text->getBox());
-  }
-
-  void setPosition(Vector p) override {
-    Control::setPosition(p);
-    this->text->setPosition(p);
-  }
-
-  void onSubmit() override { this->callback(); }
+  Button(Scene* scene, const std::string label, std::function<void()> onSubmit);
+  auto getLabel() -> std::string override;
+  void setLabel(const std::string l) override;
+  void setPosition(Vector p) override;
+  void onSubmit() override;
 };
+
 }  // namespace lbui

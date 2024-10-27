1ab083b488669618c7cbf7d58599074f6efd5b64
Manuel Spagnolo <manuel.spagnolo@proton.me>
2024-10-27 02:07:26 +0200

    docs: document Rectangle class

--- latebit/ui/Rectangle.h
+++ latebit/ui/Rectangle.h
@@ -6,9 +6,15 @@ using namespace lb;
 
 namespace lbui {
 struct RectangleOptions {
+  // Height of the rectangle
   uint8_t height = 0;
+  // Width of the rectangle
   uint8_t width = 0;
+  // Color of the rectangle
   Color::Color fill = Color::UNDEFINED_COLOR;
+  // The pattern for the border of the rectangle. Every item represents a
+  // 1-cell-wide border. So to have a border that is RED and twice as thick
+  // BLUE, you specify, {RED, BLUE, BLUE}.
   Keyframe stroke = {};
 };
 

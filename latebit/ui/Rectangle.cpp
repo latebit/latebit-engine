7815c6e2bfc32c83f7a2ac278f3d4d4a7364acc3
Manuel Spagnolo <manuel.spagnolo@proton.me>
2024-11-24 23:49:06 +0100

    chore: remove unnecessary import

--- latebit/ui/Rectangle.cpp
+++ latebit/ui/Rectangle.cpp
@@ -1,10 +1,8 @@
 #include "Rectangle.h"
 
 #include "latebit/core/geometry/Vector.h"
-#include "latebit/core/graphics/Animation.h"
 #include "latebit/core/graphics/Colors.h"
 #include "latebit/core/graphics/DisplayManager.h"
-#include "latebit/core/world/Object.h"
 
 namespace lbui {
 Rectangle::Rectangle(RectangleOptions options) : options(std::move(options)) {
